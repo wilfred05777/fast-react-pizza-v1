c8de8d387fac2d6b252f1f5a6d687c99bb667a79
Wilfred <wilfred05777@gmail.com>
2023-06-07 16:32:14 +0800

    285. A New Way Of Implementing Routes - bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
 // import React from 'react'
 
-const App = () => {
-  const x = 23
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
-  return (
-    <div>
-      <h2>HEllo react app {x}</h2>
-    </div>
-  )
+import Home from './ui/Home'
+import Menu from './features/menu/Menu'
+import Cart from './features/cart/Cart'
+import CreateOrder from './features/order/CreateOrder'
+import Order from './features/order/Order'
+// import User from './features/user/User'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/menu',
+    element: <Menu />
+  },
+  {
+    path: '/cart',
+    element: <Cart />
+  },
+  {
+    path: '/order/new',
+    element: <CreateOrder />
+  },
+  {
+    path: '/order/:orderId',
+    element: <Order />
+  }
+  // {
+  //   path: '/user',
+  //   element: <User />
+  // }
+])
+
+const App = () => {
+  return <RouterProvider router={router} />
 }
 
 export default App
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,38 +1,38 @@
-import { useState } from "react";
+import { useState } from 'react'
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
     str
-  );
+  )
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
-    totalPrice: 32,
+    totalPrice: 32
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
-    totalPrice: 13,
+    totalPrice: 13
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
-    totalPrice: 15,
-  },
-];
+    totalPrice: 15
+  }
+]
 
 function CreateOrder() {
   // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const cart = fakeCart
 
   return (
     <div>
@@ -41,32 +41,32 @@ function CreateOrder() {
       <form>
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type='text' name='customer' required />
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type='tel' name='phone' required />
           </div>
         </div>
 
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input type='text' name='address' required />
           </div>
         </div>
 
         <div>
           <input
-            type="checkbox"
-            name="priority"
-            id="priority"
+            type='checkbox'
+            name='priority'
+            id='priority'
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor='priority'>Want to yo give your order priority?</label>
         </div>
 
         <div>
@@ -74,7 +74,7 @@ function CreateOrder() {
         </div>
       </form>
     </div>
-  );
+  )
 }
 
-export default CreateOrder;
+export default CreateOrder
